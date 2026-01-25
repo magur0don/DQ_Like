2cd121e2c0b15f3f3cf0a28db79c301fc7bfd405
magur0don <magurodon.10000yen@gmail.com>
2026-01-25 12:55:15 +0900

    NPC_01の挙動を設定

--- Assets/Scripts/Field/NPCInteract.cs
+++ Assets/Scripts/Field/NPCInteract.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class NPCInteract : MonoBehaviour, IInteractable
 {
@@ -7,8 +8,16 @@ public class NPCInteract : MonoBehaviour, IInteractable
     [TextArea]
     public string TalkMessage = "‚±‚ñ‚É‚¿‚Í";
 
+    /// <summary>
+    /// UnityEvent‚Íˆ—‚ğUnityEditor‚©‚çİ’è‚Å‚«‚Ü‚·
+    /// </summary>
+    public UnityEvent NPCEvent;
+
     public void Interact()
     {
         Debug.Log($"[NPC] {TalkMessage} „‚Í{NPCName}‚Å‚·");
+        // NPCEvent‚ªİ’è‚³‚ê‚Ä‚¢‚ê‚Î(Null‚¶‚á‚È‚©‚Á‚½‚ç)A
+        // İ’è‚³‚ê‚½ˆ—‚ğ”­“®‚·‚é
+        NPCEvent?.Invoke();
     }
 }
